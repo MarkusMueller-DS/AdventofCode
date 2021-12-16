a457f4b7e80b4137da23f2aa64c8bd6adc431d34
Markinger <markus.j.mueller94@icloud.com>
2021-12-16 11:14:21 +0100

    really close to solving P2

--- 2021/Day14/Day_14_P2.py
+++ 2021/Day14/Day_14_P2.py
@@ -5,7 +5,7 @@ file = "data14.txt"
 test_file = "test_data14.txt"
 
 data = []
-with open(test_file, "r") as f:
+with open(file, "r") as f:
     for line in f:
         data.append(line.strip())
 
@@ -16,49 +16,74 @@ insertions = data[2:]
 # print(input_string)
 # print(insertions)
 
+# create defaultdict with pairs for input_string
+start_dict = defaultdict(int)
+for pair in range(len(input_string)-1):
+    pair_ = input_string[pair:pair+2]
+    start_dict[pair_] = 1
+
+# print(start_dict)
+
+
 # create defaultdict for rules
-rules_dict = defaultdict(int)
+rules_dict = defaultdict()
 for rules in insertions:
-    rules_dict[rules[:2]] = 0
+    key_ = rules[:2]
+    value_ = rules[-1]
+    rules_dict[key_] = value_
 
 # print(rules_dict)
 
 
-def find_and_replace(input_str):
-    for pair in range(len(input_str)-1):
-        for ins in insertions:
-            if input_str[pair:pair+2] == ins[:2]:
-                key = input_str[pair:pair+2] 
-                rules_dict[key] == 0
-                str_1 = key[0] + ins[-1]
-                rules_dict[str_1] += 1
-                str_2 = ins[-1] + key[1]
-                rules_dict[str_2] += 1
-    retrun rules_dict
+def find_and_replace(str_dict_count):
+    new_str_dict_count = cp.copy(str_dict_count)
+    for item in str_dict_count.items():
+        for rule in rules_dict.items():
+            if item[0] == rule[0]:
+                # print("item: ", item, " rule: ", rule)
+                # how many of are there
+                num_occs = str_dict_count[item[0]]
+                new_str_dict_count[item[0]] -= num_occs
+                # create new pairs
+                new_pair1 = item[0][0] + rule[1]
+                new_pair2 = rule[1] + item[0][1]
+                # print("creates tow new pairs: ", new_pair1, new_pair2)
+                # add to new dict
+                new_str_dict_count[new_pair1] += num_occs 
+                new_str_dict_count[new_pair2] += num_occs
+                # print(new_str_dict_count)
+                break
+    # remove keys with value 0
+    for k, v in list(new_str_dict_count.items()):
+        if v == 0:
+            del new_str_dict_count[k]
+    return new_str_dict_count
 
 
-for i in range(2):
-    dict_ = find_and_repalce(input_str)
 
 
+for i in range(40):
+   start_dict = find_and_replace(start_dict)
+   # print(star1_dict)
 
-find_and_replace(input_string)
-print(rules_dict)
+# count
+count_dict = defaultdict(int)
+for pair in start_dict.items():
+    count_dict[pair[0][0]] += pair[1]
+    count_dict[pair[0][1]] += pair[1]
 
+# every char is counted twice
+# only the char at the start and end the end is counted once
+# to make it easy to process: increse these by one and devide by 2
+# start and end chars are alwayse the same
+count_dict[input_string[0]] += 1
+count_dict[input_string[-1]] += 1
 
-#
-# # use counter so count occurences of chars
-# c = Counter(test_str)
-# print(c.most_common())
-#
-#
-#
-# min_ = 999999999999999999999
-# max_ = 0
-# for key in c.keys():
-#     if c[key] > max_:
-#         max_ = c[key]
-#     elif c[key] < min_:
-#         min_ = c[key]
-#
-# print(max_ - min_)
+count_list = []
+for count in count_dict.items():
+    count_dict[count[0]] = count[1] // 2
+    count_list.append(count[1] // 2)
+
+print(count_dict)
+
+print(max(count_list) - min(count_list))
